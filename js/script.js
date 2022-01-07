810b6e19df8d9a1a5ed8b050a41233068ab1a597
Vanesa G <87158362+vanesa5@users.noreply.github.com>
2022-01-07 11:31:15 -0500

    Created an Input function that contains conditional statemnts to only allow letters(a-z) in input box.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Global Variables 
 const unorderedList = document.querySelector(".guessed-letters");
 const button = document.querySelector(".guess");
-const textBox = document.querySelector(".letter");
+const textBox = document.querySelector(".letter"); // Text input where player inputs their letter guess
 const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuesses = document.querySelector(".remaining");
 const totalGuesses = document.querySelector(".remaining span");
@@ -15,8 +15,10 @@ const placeholder = function () {
     const placeholderForLetters = [];
     for (const letters of word) {
         console.log(letters);
-        placeholderForLetters.push("●")
-    }
+         placeholderForLetters.push("●")
+         //.unshift() - Add one or more elements to the beginning of an array.
+         //.push() - Add one or more elements to the end of an array.
+     }
     wordInProgress.innerText = placeholderForLetters.join("");
 
 };
@@ -28,5 +30,26 @@ button.addEventListener("click", function (e){
     const guess = textBox.value;
     console.log(guess);
     textBox.value = "";
-
 })
+
+//Checking Players Input Function (verifiedInput)
+const verifyInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+
+    // If there was no letter input
+    if (input.length === 0){
+        message.innerText = "Please enter a Letter(A-Z)."
+    } else if (input.length > 1) {
+        message.innerText = "Please, only enter one Letter(A-Z).";
+    } else if (!input.length.match(acceptedLetter)) {
+        message.innerText = "Synbols/Numbers are not accepted. Please enter a Letter(A-Z).";
+    } else {
+        return input; //If a input was a letter A-Z
+    }
+};
+
+// // Added New Variable to top, by word variable (guessedLetters)
+// //      const guessedLetters = []
+
+// //Capture Input Function
+// //const makesGuess = function (acceptedLetter)
